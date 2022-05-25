7037c663a3c75441165a942217507097515c786c
Bill Veiga <gabrielbillveiga@gmail.com>
2022-05-25 17:44:34 -0300

    semana1-gabrielveiga

---
+++ comex-main/src/main/java/br/com/alura/comex/Main.java
@@ -0,0 +1,31 @@
+package br.com.alura.comex;
+
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.*;
+
+import br.com.alura.comex.processadores.ProcessadorDeCsv;
+import br.com.alura.comex.relatorios.RelatorioSintetico;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		ProcessadorDeCsv processadorDeCsv = new ProcessadorDeCsv();
+		String arquivoCSV = "pedidos.csv";
+		List<Pedido> listaPedidos;
+		listaPedidos = processadorDeCsv.listarPedidos(arquivoCSV);
+        RelatorioSintetico relatorioSintetico = new RelatorioSintetico(listaPedidos);
+            
+        System.out.println("#### RELATÓRIO DE VALORES TOTAIS");
+        System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", relatorioSintetico.getTotalPedidosRealizados(listaPedidos));
+        System.out.printf("- TOTAL DE PRODUTOS VENDIDOS: %s\n", relatorioSintetico.getTotalProdutosVendidos(listaPedidos));
+        System.out.printf("- TOTAL DE CATEGORIAS: %s\n", relatorioSintetico.getTotalCategorias(listaPedidos));
+        System.out.printf("- MONTANTE DE VENDAS: %s\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")) .format(relatorioSintetico.getMontanteVendas(listaPedidos).setScale(2, RoundingMode.HALF_DOWN)));
+        System.out.printf("- PEDIDO MAIS BARATO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")) .format(relatorioSintetico.getPedidoMaisBarato(listaPedidos).getValorTotal()), relatorioSintetico.getPedidoMaisBarato(listaPedidos).getProduto());
+        System.out.printf("- PEDIDO MAIS CARO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")) .format(relatorioSintetico.getPedidoMaisCaro(listaPedidos).getValorTotal()), relatorioSintetico.getPedidoMaisCaro(listaPedidos).getProduto());
+    }
+
+
+}
---
+++ comex-main/src/main/java/br/com/alura/comex/relatorios/RelatorioSintetico.java
@@ -0,0 +1,102 @@
+package br.com.alura.comex.relatorios;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.function.Function;
+
+import br.com.alura.comex.*;
+
+public class RelatorioSintetico {
+	
+	Object totalProdutosVendidos = 0;
+    Object totalDePedidosRealizados = 0;
+    BigDecimal montanteDeVendas = BigDecimal.ZERO;
+    Pedido pedidoMaisBarato = null;
+    Pedido pedidoMaisCaro = null;
+    Object totalDeCategorias = 0;
+    
+    CategoriasProcessadas categoriasProcessadas = new CategoriasProcessadas();
+    
+
+    
+    	public RelatorioSintetico(List<Pedido> listaPedidos) {
+            super();
+            if (listaPedidos == null || listaPedidos.isEmpty()) throw new IllegalArgumentException("A lista de pedidos não pode ser vazia!");
+
+            this.totalDePedidosRealizados = listaPedidos.size();
+            Function<Pedido, BigDecimal> mapaPedidos = pedido -> pedido.getValorTotal();
+           
+            this.totalProdutosVendidos = listaPedidos
+            		.stream()
+            		.mapToInt(pedido -> pedido.getQuantidade()) 
+            		.sum();
+            listaPedidos.forEach(pedido ->categoriasProcessadas.add(pedido.getCategoria()));
+            
+            this.totalDeCategorias = getTotalCategorias(listaPedidos);
+            
+            this.montanteDeVendas = listaPedidos
+            		.stream()
+            		.map(mapaPedidos) 
+            		.reduce(BigDecimal.ZERO, BigDecimal::add);
+            this.pedidoMaisBarato = getPedidoMaisBarato(listaPedidos);
+            this.pedidoMaisCaro = getPedidoMaisCaro(listaPedidos);
+     
+    	}	
+    	
+    
+
+
+
+
+
+    	public Pedido getPedidoMaisCaro(List<Pedido> listaPedidos) {
+            return listaPedidos.stream().max(Comparator.comparing(Pedido::getValorTotal)).orElseThrow(() -> new IllegalStateException("A lista de pedidos não deveria estar vazia."));
+        }
+
+
+	public void setPedidoMaisBarato(Pedido pedidoMaisBarato) {
+		// TODO Auto-generated method stub
+		this.pedidoMaisBarato = pedidoMaisBarato;
+	}
+	
+	public Pedido getPedidoMaisBarato(List<Pedido> listaPedidos) {
+        return listaPedidos.stream().min(Comparator.comparing(Pedido::getValorTotal)).orElseThrow(() -> new IllegalStateException("A lista de pedidos não deveria estar vazia."));
+    }
+
+	public BigDecimal getMontanteVendas(List<Pedido> listaPedidos) {
+        Function<Pedido, BigDecimal> mapaPedidos = pedido -> pedido.getValorTotal();
+        return montanteDeVendas = listaPedidos
+                .stream()
+                .map(mapaPedidos)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+	}
+
+
+	public int getTotalCategorias(List<Pedido> listaPedidos) {
+        listaPedidos.forEach(pedido -> categoriasProcessadas.add(pedido.getCategoria()));
+        return categoriasProcessadas.size();
+    }
+
+
+	public Object getTotalProdutosVendidos(List<Pedido> listaPedidos) {
+        return this.totalProdutosVendidos = listaPedidos
+                .stream()
+                .mapToInt(pedido -> pedido.getQuantidade())
+                .sum();
+    }
+
+
+	public int getTotalPedidosRealizados(List<Pedido> listaPedidos) {
+        return listaPedidos.size();
+    }
+
+
+}
+    
